0b5a028adb7c3a033f35a9d41173a630a3c5c49f
dhbart <dhbart88@gmail.com>
2022-10-25 20:23:09 -0300

    Adicionado Terraform

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+    required_providers {
+       aws = ">=4.34.0"
+      local = ">=2.2.3"
+    }
+    backend "s3" {
+      bucket = "myfcbucket-dhbart"
+      key    = "terraform.tfstate"
+      region = "us-east-1"
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "new-vpc" {
+  source = "./modules/vpc"
+  prefix = var.prefix
+  vpc_cidr_block = var.vpc_cidr_block
+}
+
+module "eks" {
+    source = "./modules/eks"
+    prefix = var.prefix
+    vpc_id = module.new-vpc.vpc_id
+    cluster_name = var.cluster_name
+    retention_days = var.retention_days
+    subnet_ids = module.new-vpc.subnet_ids
+    desired_size = var.desired_size
+    max_size = var.max_size
+    min_size = var.min_size
+}
